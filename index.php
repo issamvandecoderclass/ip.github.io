641e6d4ebfbd966b5f1f481a1a11676099c64617
issam <issam.buamran@coderclass.nl>
2017-10-11 13:36:33 +0200

    Hij laat nu de ip zien van de gebruiker

--- index.php
+++ index.php
@@ -3,5 +3,12 @@
     <head>
       <title>Ip checker</title>
     </head>
+    <body>
+      <?php
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR']
+        echo $ip
+      ?>
+
+    </body>
 
   </html>
